f93d67943fc74e23bd607671f73d30ae1f4d4450
ajhalim <thenubbum@gmail.com>
2023-10-10 10:43:11 -0700

    testing
    testing

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "My game";
 
 document.title = gameName;
-
+//testiong
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
