4f66b0b5ca2641c857fa4b12c7a471df370f98ba
ajhalim <thenubbum@gmail.com>
2023-10-10 11:55:04 -0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Abe Game";
 
 document.title = gameName;
-//testiong
+//testiongdasfasfas
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
