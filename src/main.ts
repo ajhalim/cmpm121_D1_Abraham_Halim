5e25fded5e493b85f7bcb156fe0b4455d4678b34
ajhalim <thenubbum@gmail.com>
2023-10-11 15:45:14 -0700

    step 1

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,19 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+
+const button: HTMLButtonElement = document.createElement("button");
+button.textContent = "☠";
+
 app.append(header);
+
+let count: number = 0;
+const countDisplay: HTMLDivElement = document.createElement("div");
+countDisplay.innerText = `You have summoned ${count} bones!`;
+
+button.addEventListener("click", () => {
+  count++;
+  countDisplay.innerText = `You have summoned ${count} bones!!`;
+});
+
+app.append(header, button, countDisplay);
